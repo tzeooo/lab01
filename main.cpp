108de24ae2e4e41f1a6badf8273d61e279e21b18
tzeooo <burdujacristian@mail.ru>
2024-10-03 16:02:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
 #include "gameengine.hpp"
 
 int main() {
-
     GameEngine game;
-
-
     game.Init();
-
-    std::cout << "Jocul a startat!" << std::endl;
-
- 
     game.Run();
-
     return 0;
 }
